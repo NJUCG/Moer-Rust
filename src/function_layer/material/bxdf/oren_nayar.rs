ffbca41cef4b8a2e3c5a80abb46549d73a6f7dbb
Indigo <ysu@smail.nju.edu.cn>
2023-04-27 14:53:26 +0800

    The framework for lab2

--- src/function_layer/material/bxdf/oren_nayar.rs
+++ src/function_layer/material/bxdf/oren_nayar.rs
@@ -26,9 +26,10 @@ impl BSDF for OrenNayarBSDF {
     fn f(&self, wo: V3f, wi: V3f) -> SpectrumRGB {
         let wo_local = self.to_local(wo);
         let wi_local = self.to_local(wi);
-        let s = self.sigma;
-        let a = 1.0 - 0.5 * s * s / (s * s + 0.33);
-        let b = 0.45 * s * s / (s * s + 0.09);
+        let s2 = self.sigma * self.sigma;
+        let a = 1.0 - 0.5 * s2 / (s2 + 0.33);
+        let b = 0.45 * s2 / (s2 + 0.09);
+
         let sin_alpha = (1.0 - wo_local.y * wo_local.y).sqrt().
             max((1.0 - wi_local.y * wi_local.y).sqrt());
         let tan_beta = (1.0 / (wo_local.y * wo_local.y) - 1.0).sqrt().
@@ -38,7 +39,7 @@ impl BSDF for OrenNayarBSDF {
             (wo_local.x * wo_local.x + wo_local.z * wo_local.z).sqrt() /
             (wi_local.x * wi_local.x + wi_local.z * wi_local.z).sqrt();
 
-        self.albedo * INV_PI * (a + b * cos_dphi.max(0.0) * sin_alpha * tan_beta)
+        self.albedo * INV_PI * wi_local.y * (a + b * cos_dphi.max(0.0) * sin_alpha * tan_beta)
     }
 
     fn sample(&self, _wo: V3f, sample: Vector2<f32>) -> BSDFSampleResult {
--- src/function_layer/material/mod.rs
+++ src/function_layer/material/mod.rs
@@ -3,5 +3,11 @@ pub mod material;
 pub mod matte;
 mod mirror;
 mod phong;
+mod oren_nayar;
+mod ndf;
+mod conductor;
+mod dielectric;
 
 pub use material::Material;
+pub use ndf::NDF;
+pub use bxdf::BSDF;
